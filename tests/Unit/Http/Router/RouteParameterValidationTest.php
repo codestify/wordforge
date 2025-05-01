9980c4d00205c62ff4a525da676438bf3c0c2846
alimanza <muhammad.ali@verge.digital>
2025-05-01 01:00:39 +0100

    fix code style

--- tests/Unit/Http/Router/RouteParameterValidationTest.php
+++ tests/Unit/Http/Router/RouteParameterValidationTest.php
@@ -14,26 +14,26 @@ class RouteParameterValidationTest extends TestCase
     {
         // Create a route
         $route = new Route(['GET'], 'posts/{post_id}', 'PostController@show', 'wordforge/v1');
-        
+
         // Set a problematic pattern (pattern containing characters that need escaping)
         $route->where('post_id', '[0-9]+');
-        
+
         // Use reflection to access the protected validateParameter method
         $reflectionClass = new \ReflectionClass($route);
         $method = $reflectionClass->getMethod('validateParameter');
         $method->setAccessible(true);
-        
+
         // This should not throw a preg_match error
         $result = $method->invoke($route, 'post_id', '12345');
-        
+
         // The method should return true for a valid parameter value
         $this->assertTrue($result);
-        
+
         // Also test against an invalid value
         $resultInvalid = $method->invoke($route, 'post_id', 'abc');
         $this->assertFalse($resultInvalid);
     }
-    
+
     /**
      * Test to verify that the regex error is fixed for other special characters too
      */
@@ -41,7 +41,7 @@ class RouteParameterValidationTest extends TestCase
     {
         // Create a route
         $route = new Route(['GET'], 'test/{param}', 'TestController@show', 'wordforge/v1');
-        
+
         // Test with various special characters that could cause regex issues
         $problematicPatterns = [
             '[0-9]+',            // Square brackets
@@ -55,14 +55,14 @@ class RouteParameterValidationTest extends TestCase
             '\\d+',              // Backslash
             '$[a-z]^'            // Multiple special chars
         ];
-        
+
         $reflectionClass = new \ReflectionClass($route);
         $method = $reflectionClass->getMethod('validateParameter');
         $method->setAccessible(true);
-        
+
         foreach ($problematicPatterns as $pattern) {
             $route->where('param', $pattern);
-            
+
             try {
                 // Just verify that it doesn't throw an exception
                 $method->invoke($route, 'param', 'test123');
